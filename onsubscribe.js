b36d862a525d54f096ed4572348f787d91df63f8
polinens <polineni.srikanth@gmail.com>
2019-09-09 22:13:06 +0530

    Create onsubscribe.js

---
+++ onsubscribe.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({
+    apiVersion: '2012-08-10'
+});
+
+exports.handler = async(event, context) => {
+
+    const connectionId = event.requestContext.connectionId;
+    const event = JSON.parse(event.body).event;
+
+    if (!event) {
+        return {
+            statusCode: 400,
+            body: "Bad request"
+        };
+    }
+
+    const {
+        TABLE_NAME
+    } = process.env;
+
+    var params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'c_id': connectionId,
+            'event': event,
+            'ttl': Math.floor(Date.now() / 1000) + 3600
+        },
+        ReturnValues: 'NONE'
+    };
+
+    try {
+        const data = await docClient.put(params).promise();
+        return {
+            statusCode: 200,
+            body: "Subscribed for: " + event
+        };
+    }
+    catch (e) {
+        console.log(e.stack);
+        return {
+            statusCode: 500,
+            body: "Failed to Subscribe"
+        };
+    }
+};
