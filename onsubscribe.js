bada7ba68aa822a3305dd4008cb42d996f132d9a
polinens <polineni.srikanth@gmail.com>
2019-09-09 22:55:06 +0530

    Update onsubscribe.js

--- onsubscribe.js
+++ onsubscribe.js
@@ -10,13 +10,6 @@ exports.handler = async(event, context) => {
     const connectionId = event.requestContext.connectionId;
     const topic = JSON.parse(event.body).topic;
 
-    if (!event) {
-        return {
-            statusCode: 400,
-            body: "Bad request"
-        };
-    }
-
     const {
         TABLE_NAME
     } = process.env;
@@ -24,25 +17,15 @@ exports.handler = async(event, context) => {
     var params = {
         TableName: TABLE_NAME,
         Item: {
-            'c_id': connectionId,
-            'topic': topic,
-            'ttl': Math.floor(Date.now() / 1000) + 3600
+            'connectionId': connectionId,
+            'topic': topic
         },
         ReturnValues: 'NONE'
     };
 
-    try {
-        const data = await docClient.put(params).promise();
-        return {
-            statusCode: 200,
-            body: "Subscribed for: " + event
-        };
-    }
-    catch (e) {
-        console.log(e.stack);
-        return {
-            statusCode: 500,
-            body: "Failed to Subscribe"
-        };
-    }
+    const data = await docClient.put(params).promise();
+    return {
+        statusCode: 200,
+        body: "Subscribed for: " + event
+    };
 };
