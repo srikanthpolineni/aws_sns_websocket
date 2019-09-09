16f02940532b7c63bc46e87c3278b450a97569b7
polinens <polineni.srikanth@gmail.com>
2019-09-09 22:53:49 +0530

    Update onpublish.js

--- onpublish.js
+++ onpublish.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient({
     apiVersion: '2012-08-10'
@@ -12,75 +11,34 @@ exports.handler = async(event, context) => {
         CALL_BACK_URL
     } = process.env;
 
-    let message;
-    try {
-        message = JSON.parse(event.Records[0].Sns.Message);
-    }
-    catch (e) {
-        console.log(e.stack);
-        console.log('Recieved invalid event notification from SNS.');
-        return;
-    }
+    let message = JSON.parse(event.Records[0].Sns.Message);
+   
     const topic = message.topic;
     const postData = message.data;
-    let connectionData;
-    try {
-        connectionData = await docClient.scan({
+    let connectionData = await docClient.scan({
             TableName: TABLE_NAME,
-            ProjectionExpression: "c_id",
+            ProjectionExpression: "connectionId",
             FilterExpression: "topic = :topic",
             ExpressionAttributeValues: {
               ":topic": topic
             }
         }).promise();
-
-    }
-    catch (e) {
-        console.log(e.stack);
-        return;
-    }
-    console.log(connectionData);
     const apigwManagementApi = new AWS.ApiGatewayManagementApi({
         apiVersion: '2018-11-29',
         endpoint: CALL_BACK_URL
     });
 
     const postCalls = connectionData.Items.map(async({
-        c_id
+        connectionId
     }) => {
-        try {
-            await apigwManagementApi.postToConnection({
-                ConnectionId: c_id,
+        await apigwManagementApi.postToConnection({
+                ConnectionId: connectionId,
                 Data: postData
             }).promise();
-        }
-        catch (e) {
-            if (e.statusCode === 410) {
-                console.log(`Found stale connection, deleting ${c_id}`);
-                await docClient.delete({
-                    TableName: TABLE_NAME,
-                    Key: {
-                        "c_id": c_id
-                    }
-                }).promise();
-            }
-            else {
-                console.log(e.stack);
-                throw e;
-            }
-        }
     });
 
-    try {
-        await Promise.all(postCalls);
-    }
-    catch (e) {
-        return {
-            statusCode: 500,
-            body: e.stack
-        };
-    }
-
+    await Promise.all(postCalls);
+   
     return {
         statusCode: 200,
         body: 'Message Notified.'
